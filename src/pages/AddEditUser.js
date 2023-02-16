cb192f3250b1fad8dbe7e852b6a3db7e605e1645
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-16 21:27:08 +0100

    the file upload function has been created uploadFile

--- src/pages/AddEditUser.js
+++ src/pages/AddEditUser.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { storage } from "../firebase";
+import { useParams, useNavigate } from "react-router-dom";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 let initialState = {
   name: "",
   email: "",
@@ -10,10 +12,46 @@ export default function AddEditUser() {
   const [data, setData] = useState(initialState);
   const { name, email, info, contact } = data;
   const [file, setFile] = useState(null);
-  // const [progress, setProgress] = useState(null);
+  const [progress, setProgress] = useState(null);
   const [error, setError] = useState(null);
   // const [isSubmit, setSubmit] = useState(false);
   const [emptyFields, setEmptyFields] = useState([]);
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is Pause");
+              break;
+            case "running":
+              console.log("Upload is Running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev) => ({ ...prev, img: downloadURL }));
+          });
+        }
+      );
+    };
+    file && uploadFile();
+  }, [file]);
+
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
@@ -69,7 +107,7 @@ export default function AddEditUser() {
       />
       <label>Upload</label>
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-      <button>add user</button>
+      <button disabled={progress !== null && progress < 100}>add user</button>
       {error && <div className="error">{error}</div>}
     </form>
   );
