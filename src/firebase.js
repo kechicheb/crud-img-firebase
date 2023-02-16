4be85e04379b4432254505a5d7e7aa16ed76ca78
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-16 14:11:22 +0100

    crud image use firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZr4TovguK9BvaDwGh_MAw9_RN3b9N6Kk",
+  authDomain: "crud-img-71ee9.firebaseapp.com",
+  projectId: "crud-img-71ee9",
+  storageBucket: "crud-img-71ee9.appspot.com",
+  messagingSenderId: "811444790047",
+  appId: "1:811444790047:web:96fdb379fda8d8e028862a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
