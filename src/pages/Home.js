586725525bd29a360aca16145cd8e6a39efc60f0
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-17 17:15:16 +0100

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { collection, onSnapshot } from "firebase/firestore";
 
-export default function Home() {
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setLoading(true);
+    const unsub = onSnapshot(
+      collection(db, "users"),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setUsers(list);
+        setLoading(false);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      {users &&
+        users.map((item) => (
+          <div className="card">
+            <img src={item.img} alt="Avatar" style={{ width: "100%" }} />
+            <div className="container">
+              <h4>
+                <b>{item.name}</b>
+              </h4>
+              <p>{item.email}</p>
+              
+            </div>
+            <div className="btns">
+                <button
+                  className="update btn"
+                  onClick={() => navigate(`/update/${item.id}`)}
+                >
+                  update
+                </button>
+                <button className="view btn">view</button>
+              </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+export default Home;
